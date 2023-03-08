4f6c92eee427a399a46129a2e9924d5eb646a6c1
umbcstudent <125592850+umbcstudent@users.noreply.github.com>
2023-03-08 02:36:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Show the Quiz Title
+# Show the Quiz Title
 print(r"""
 ╔╗ ╔╗                   ╔╗        ╔═══╗           
 ║║ ║║                   ║║        ║╔═╗║           
@@ -9,53 +9,65 @@ print(r"""
              ║║                      ╚╝           
              ╚╝                                   
 """)
-print("Welcome to Harper's Quiz! You will answer three True or False questions. Please ONLY enter the following letter T or F. Good Luck! ")
+print(
+    "Welcome to Harper's Quiz! You will answer three True or False questions. Please ONLY enter the following letter T or F. Good Luck! ")
 
+# Setup Inquiry
+inquiry = (
+'Q1. Light travels in space goes in a straight line.', 'Q2. The Mona Liza was stolen from the Louvre in 1911.',
+'Q3. Are greyhound faster than cheetah?')
 
-#Setup Inquiry 
-inquiry = ('Q1. Light travels in space goes in a straight line.', 'Q2. The Mona Liza was stolen from the Lourve in 1911.', 'Q3. Are greyhound faster than cheetah?')
-
-#Setup Answers
+# Setup Answers
 answers = ('T', 'T', 'F')
-correctAnswer = 1
+correctAnswer = 0
 
-#Show the user question 1
+# Calculate the total right answers
+numberOfInquiry = len(inquiry)
+
+# Show the user question 1
 print(inquiry[0])
-#Get the answer from the user
-userGuess = input("Please enter 'T' for true or 'F' for false ")
-userGuess = int(len(userGuess))
-#Compare the answer to the correct answers
-if(answers == answers[0]):
-  #Print a blank space
-  print()
-#Show the user question 2
-print(inquiry[1])
-#Get the answer from the user
-userGuess = input("Please enter 'T' for true or 'F for false. ")
-userGuess = int(len(userGuess))
-#Compare the answer to the correct answers
-if(answers == answers[1]):
-  #Print a blank space
-  print()
-#Show the user question 3
-print(inquiry[2])
-#Get the answer from the user
-userGuess = input("Please enter 'T' for true or 'F' for false. ")
-userGuess = int(len(userGuess))
-if(answers == answers[2]):
-  #Print a blank space
-  print()
-
-#Calculate the total number of inquiry
-numberOfInquiry = len(inquiry) 
-
-#Add to correct answers
-correctAnswer += 1
-
-#Loop once for each inquiry
+# Get the answer from the user
+userGuess = input("Please enter 'T' for true or 'F'' for false: ")
+# Create awhile loop that runs until 'T' or 'F'
+while userGuess not in answers:
+    # Prompt user for a new answer
+    userGuess = input("Please enter 'T' for true or 'F' for false: ")
+#Create a loop that scans each question
 for index in range(numberOfInquiry):
-    print(f'You got {correctAnswer} out of {len(inquiry)} correct! Thanks for playing!')
-  
-#Print a blank space
-print()
+    #Compare the answers to the correct answers
+    if userGuess == answers[0]:
+        # Add a point for correct answer
+        correctAnswer += 1
+        
 
+# Show the user question 2
+print(inquiry[1])
+# Get the answer from the user
+userGuess = input("Please enter 'T' for true or 'F for false: ")
+# Create awhile loop that runs until 'T' or 'F'
+while userGuess not in answers:
+    # Prompt user for a new answer
+    userGuess = input("Please enter 'T' for true or 'F' for false. ")
+    #Compare the answer to the correct answers
+    if userGuess == answers[1]:
+        # Add a point for correct answer
+        correctAnswer += 1
+        # Print a blank space
+
+# Show the user question 3
+print(inquiry[2])
+# Get the answer from the user
+userGuess = input("Please enter 'T' for true or 'F' for false. ")
+# Create awhile loop that runs until 'T' or 'F'
+while userGuess not in answers:
+    # Prompt user for a new answer
+    userGuess = input("Please enter 'T' for true or 'F' for false. ")
+    # Compare the answer to the correct answers
+    if  userGuess == answers[2]:
+        # Add a point for correct answer
+        correctAnswer += 1
+
+       
+# Loop once for each inquiry
+for index in range(numberOfInquiry != 1):
+    print(f'You got {correctAnswer} out of {numberOfInquiry} correct! Thanks for playing!')
